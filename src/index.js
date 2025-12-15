c9979fc00505c45bb602297bfd750c7fa0ccbdc9
Jothikumar <35065074+Jothikumarsk@users.noreply.github.com>
2025-12-15 15:20:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // remove if you don't have a stylesheet
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
